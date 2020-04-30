74223c1e5b3aab3c2e49660eaad7fe6592cbed23
Shokhrukh <mr.shox1998@gmail.com>
2020-04-30 23:55:39 +0900

    Midterm_18012762

---
+++ Midterm_18012762_Abdusalyamov_Shokhrukh.R
@@ -0,0 +1,39 @@
+my_fullname <- "Abdusalyamov Shokhrukh"
+my_std_no <- 18012762
+ 
+# Part 1:
+
+#  1. What does CRAN stand for ?
+
+cat('What does CRAN stand for ?')
+cat(' Definition of CRAN is Comprehensive R Archive Network (R programming language).')
+
+# 2. What is the most important thing in Data Science?
+cat('What is the most important thing in Data Science?')
+cat('In Data Sience, Using the right software is the most important thing.')
+
+# 3. I am done editing a file, I need to _______ those changes then _______ them, and _______ it to the __________ .
+cat('I am done editing a file, I need to ______ those changes then _______ them, and _______ it to the __________ .')
+cat('I am done editing a file, I need to Commit those changes then merge them, and push it to the repository.')
+
+
+
+# Part 2
+
+#
+setwd("~/Desktop/midterm_exam")
+ds.titanic.survival <- read.csv("TitanicSurvival.csv", stringsAsFactors = FALSE)
+
+# Answers
+
+
+cat('The number of column is 5')
+
+cat('The number of rows is 1310')
+
+#1.  What is the rate of female and male in Titanic Survival data set? 
+
+
+
+cat('The famale rate is ')
+
